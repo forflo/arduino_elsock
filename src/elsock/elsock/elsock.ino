fe4b812ab3a087f09fef9ae5d8d6411cd012eb46
Florian <mayerflorian@web.de>
2014-09-27 15:04:16 +0200

    First (untested) prototype for elsock. (Good luck)

--- src/elsock/elsock/elsock.ino
+++ src/elsock/elsock/elsock.ino
@@ -1,17 +1,27 @@
+#include <SPI.h>
 #include <Ethernet.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }
-IPAddress fallback_ip(141,60,125,4)
-IPAddress fallback_gateway(141,60,125,1)
-IPAddress fallback_subnet(255,255,255,0)
+String ports[] = {"null", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+int status[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int portnum = 10;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress fallback_ip(141,60,125,4);
+IPAddress fallback_gateway(141,60,125,1);
+IPAddress fallback_subnet(255,255,255,0);
 
 EthernetServer server(7878);
 boolean rcvd_msg = false;
 
 void setup(){
+  /* configure pins */
+  for(int i = 0; i < portnum; i++){
+    pinMode(i, OUTPUT);
+  }
+  
+  /* configure the serial connection */
   Serial.begin(9600);
 
-  Serial.println("[setup()] Trying to query configuration by using DHCP")
+  Serial.println("[setup()] Trying to query configuration by using DHCP");
   if (Ethernet.begin(mac) == 0){
     Serial.println("[setup()] Failed to get DHCP-IP address");
     Serial.println("... using fallback configuration instead");
@@ -19,7 +29,7 @@ void setup(){
   }
   
   Serial.print("[setup()] IP-address: ");
-  ip = Ethernet.localIP();
+  IPAddress ip = Ethernet.localIP();
   for (byte thisByte = 0; thisByte < 4; thisByte++){
     Serial.print(ip[thisByte], DEC);
     Serial.print(".");
@@ -27,17 +37,19 @@ void setup(){
   Serial.println();
   
   server.begin();
+
 }
 
 void loop(){
   /* like the accept() library function from the BSD-Socketlib */
   EthernetClient client = server.available();
+  String path = "";
   
   if(client){
     String line = get_line(client);
     if (line.charAt(0) == 'G' && line.charAt(1) == 'E' && line.charAt(2) == 'T'){
       if (line.charAt(4) == '/'){
-        String path = "";
+        
         int index = 5;
         char temp;
         /* Get the Path of the request */
@@ -46,13 +58,29 @@ void loop(){
         }
       }
     }
+    
+    for (int i = 0; i < portnum; i++){
+      if(!path.compareTo(ports[i])){
+        /* Port found! */
+        status[i] = !status[i];        
+        
+        if(status){
+          digitalWrite(i, HIGH);
+        } else {
+          digitalWrite(i, LOW);
+        }
+      }
+    }
+    
+    /* collect the irrelevant headers until the double \n is reached */
+    while (get_line(client).length() != 0);
   }
 }
 
 String get_line(EthernetClient c){
   String result = "";
   char temp;
-  while ((temp = client.read()) != '\n'){
+  while ((temp = c.read()) != '\n'){
     result += temp;
   }
   
