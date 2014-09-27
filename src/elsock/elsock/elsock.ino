f15fa3733ac344c613139d2e201b043644c45ee0
Florian <mayerflorian@web.de>
2014-09-27 23:03:08 +0200

    This snapshot currently has one interesting bug. If the lines 23, 24 and 53 are commented out, the whole program runs as expected. However, if these lines are compiled into the binary, strange things happen.

--- src/elsock/elsock/elsock.ino
+++ src/elsock/elsock/elsock.ino
@@ -1,13 +1,17 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
 
 String ports[] = {
-  "null", "one", "two", 
-  "three", "four", "five", 
-  "six", "seven", "eight", "nine"
+  "nul", "one", "two", 
+  "thr", "fou", "fiv", 
+  "six", "sev", "eig", "nin"
 };
 
-int status[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+byte status[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
 int portnum = 10;
 byte mac[] = { 
   0xDE, 0xAD, 0xBE, 
@@ -15,12 +19,14 @@ byte mac[] = {
 };
 
 IPAddress fallback_ip(141,60,125,4);
+
 IPAddress fallback_gateway(141,60,125,1);
 IPAddress fallback_subnet(255,255,255,0);
+
 String current_ip;
 
+File myFile;
 EthernetServer server(80);
-boolean rcvd_msg = false;
 
 void setup(){
   /* configure pins */
@@ -30,45 +36,57 @@ void setup(){
   
   /* configure the serial connection */
   Serial.begin(9600);
+  
+  Serial.println("[setup()] Begin SD init ...");
+  pinMode(10, OUTPUT);
+  if (!SD.begin(4)){
+    Serial.println("[setup()] SD init failed");
+    exit();
+  }
+  Serial.println("[setup()] finished!");
 
+  /* Ethernet config */
   Serial.println("[setup()] Trying to query configuration by using DHCP");
   if (Ethernet.begin(mac) == 0){
     Serial.println("[setup()] Failed to get DHCP-IP address");
     Serial.println("... using fallback configuration instead");
     Ethernet.begin(mac, fallback_ip, fallback_gateway, fallback_subnet);
   }
-  
-  Serial.print("[setup()] IP-address: ");
-  IPAddress ip = Ethernet.localIP();
-  for (byte thisByte = 0; thisByte < 4; thisByte++){
-    Serial.print(ip[thisByte], DEC);
-    Serial.print(".");
-  }
-  Serial.println();
-  
+
+  fallback_ip = Ethernet.localIP();
+
   /* set the current ip */
   current_ip = String("");
   for (byte b = 0; b < 4; b ++){
-    current_ip += String(ip[b], DEC);
+    current_ip += String(fallback_ip[b], DEC);
     if (b != 3 ) {
       current_ip += ".";
     }
   }
-    
+  
+  exit();
+  Serial.print("[setup()] current IP: ");
+  Serial.println(current_ip);
+
   server.begin();
+}
+
+void exit(){
+  Serial.println("EXIT");
+  while(true);
 }
 
 void loop(){
   /* like the accept() library function from the BSD-Socketlib */
   EthernetClient client = server.available();
-  String path = "";
-  
+  String path = String("");
+
   if(client){
     String line = get_line(client);
     Serial.println(line);
     if (line.charAt(0) == 'G' && line.charAt(1) == 'E' && line.charAt(2) == 'T'){
       if (line.charAt(4) == '/'){
-        
+
         int index = 5;
         char temp;
         /* Get the Path of the request */
@@ -77,51 +95,74 @@ void loop(){
         }
       }
     }
-    
+
     Serial.print("[server] path: ");
-    Serial.print('/'); Serial.println(path);
-    
+    Serial.print('/'); 
+    Serial.println(path);
+
     if(!path.equals("")){
       Serial.println("[server] sending webpage ...");
-      client.println(generate_webpage());
-    } else if (!path.equals("main.css")) {
+      /* static content */
+      myFile = SD.open("index.htm");
+      if(myFile){
+        while(myFile.available()){
+          client.println(myFile.read());
+        }
+        myFile.close();
+      } 
+      else {
+        Serial.println("[server] Error opening index.css");
+      }
+      /* sending dynamic content */
+      send_webpage(client);
+    } 
+    else if (!path.equals("main.css")) {
       Serial.println("[server] sending css ...");
-      client.println(generate_css());
-    } else {
-    
+
+      /* send main.css to client */
+      myFile = SD.open("main.css");
+      if(myFile){
+        while(myFile.available()){
+          client.println(myFile.read());
+        }
+        myFile.close();
+      } 
+      else {
+        Serial.println("[server] Error opening main.css");
+      }
+
+    } 
+    else {
+
       for (int i = 0; i < portnum; i++){
         if(!path.compareTo(ports[i])){
           /* Port found! */
-        
+
           if(status[i]){
             digitalWrite(i, LOW);
-          } else {
+          } 
+          else {
             digitalWrite(i, HIGH);
           }
-        
+
           status[i] = !status[i];
 
           break;
         }
       }
     }
-    
-    /* collect the irrelevant headers until the double \n is reached 
-        Attention: length() also counts the null byte at the end of the string! 
-    while (get_line(client).length() != 1);
-    Serial.println("[server] Disconnecting from client...");
-    */
+
     client.stop();
   }
 }
 
 String get_line(EthernetClient c){
-  String result = "";
+  String result = String("");
   char temp;
   while ((temp = c.read()) != '\n'){
     result += temp;
   }
-  
+
   /* debug */
   Serial.print("[server:get_line()] ");
   Serial.println(result);
@@ -129,68 +170,41 @@ String get_line(EthernetClient c){
   return result;
 }
 
-String generate_css(){
-  String css = String("");
+void send_webpage(EthernetClient c){
+  c.print("<p>");
 
-  css += ".aesock a {    border-bottom: 1px solid #777777;  border-left: 1px solid #000000;  border-right: 1px solid #333333;border-top: 1px solid #000000;color: #000000;display: block;height: 2.5em;padding: 0 1em;width: 5em;       text-decoration: none; }";   
-  css += ".aesock a:hover {    border-bottom: 1px solid #777777;   border-left: 1px solid #000000; border-right: 1px solid #333333; border-top: 1px solid #000000;color: #f0f;display: block;    height: 2.5em;padding: 0 1em;width: 5em;       text-decoration: none;}";
-  css += ".aesock a:active {    border-bottom: 1px solid #777777;    border-left: 1px solid #000000;   border-right: 1px solid #333333;    border-top: 1px solid #000000;    color: #f00;    display: block;    height: 2.5em;    padding: 0 1em;    width: 5em;       text-decoration: none; }";
-  css += ".button-link {  padding: 10px 15px;  background: #4479BA;  color: #FFF;  -webkit-border-radius: 4px;  -moz-border-radius: 4px;   border-radius: 4px;   border: solid 1px #20538D;   text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);  -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);   -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);   box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);    -webkit-transition-duration: 0.2s;   -moz-transition-duration: 0.2s;   transition-duration: 0.2s;}";
-  css += ".button-link:hover, .button-link:focus {   background: #356094;   border: solid 1px #2A4E77;text-decoration: none;}";
-  css += ".button-link:active {    -webkit-box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);    -moz-box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);    box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);    background: #2E5481;    border: solid 1px #203E5F;}";
-
-  return css;
-}
-
-String generate_webpage(){
-  String result = String("");
-  /* Fucking Arduino IDE doesn't know how to handle 
-    strings spanning over more than one line ... */
-  result += "<!DOCTYPE html>";
-  result += "<html lang=\"en\">";
-  result += "<head>";
-  result += "<meta charset=\"utf-8\">";
-  result += "<title>Electrical network socket</title>";
-  result += "<link href=\"main.css\" rel=\"stylesheet\" />";
-  result += "</head>";
-  result += "<body>";
-  result += "<h2>";
-  result += "Toggle the state of the ports by pressing the buttons below";
-  result += "</h2>";
-  result += "<br></br>";
-  result += "<p>";
-  
   /* Dynamic content */
   for (int i = 1; i < 4; i++){
-    result += "<a class=\"button-link\" href=\"";
-    result += "http://";
-    result += current_ip;
-    result += "/";
-    result += ports[i];
-    result += "\">Toggle Socket 1</a>";
+    c.print("<a class=\"button-link\" href=\"");
+    c.print("http://");
+    c.print(current_ip);
+    c.print("/");
+    c.print(ports[i]);
+    c.print("\">Socket 1</a>");
   }
-  result += "<br></br></p><p>";
-  
+  c.print("<br></br></p><p>");
+
   for (int i = 4; i < 7; i++){
-    result += "<a class=\"button-link\" href=\"";
-    result += "http://";
-    result += current_ip;
-    result += "/";
-    result += ports[i];
-    result += "\">Toggle Socket 1</a>";
+    c.print("<a class=\"button-link\" href=\"");
+    c.print("http://");
+    c.print(current_ip);
+    c.print("/");
+    c.print(ports[i]);
+    c.print("\">Socket 1</a>");
   }
-  result += "<br></br></p><p>";
-  
- for (int i = 7; i < 10; i++){
-    result += "<a class=\"button-link\" href=\"";
-    result += "http://";
-    result += current_ip;
-    result += "/";
-    result += ports[i];
-    result += "\">Toggle Socket 1</a>";
+  c.print("<br></br></p><p>");
+
+  for (int i = 7; i < 10; i++){
+    c.print( "<a class=\"button-link\" href=\"");
+    c.print("http://");
+    c.print(current_ip);
+    c.print("/");
+    c.print(ports[i]);
+    c.print("\">Socket 1</a>");
   }
-  
-  result += "</p></body></html>";
-  
-  return result;
+
+  c.print("</p></body></html>");
 }
+
+
+
