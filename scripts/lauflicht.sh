cee0795dc4b07e13159487a45d16e50336ac0858
forflo <mayerflorian@me.com>
2014-10-06 11:55:42 +0200

    Added lauflicht.sh

--- scripts/lauflicht.sh
+++ scripts/lauflicht.sh
@@ -2,9 +2,10 @@
 HOST="http://141.60.125.177"
 
 while :; do
-	for i in {a..j}; do
-	echo Toggle $i
-	curl ${HOST}/$i > /dev/null 2>&1
-	sleep 0.2
+	temp=""
+	for i in {c..j}; do
+		curl ${HOST}/p?L$temp=0"&"L$i=1 > /dev/null 
+		temp="c"
+		sleep 0.2
 	done
 done
